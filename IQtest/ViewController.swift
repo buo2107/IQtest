0baec7de8ed8ff6dfb816f5c23f82446997f7795
User16 <user16@user16de-imac.local>
2018-11-27 19:35:57 +0800

    Initial Commit

---
+++ IQtest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IQtest
+//
+//  Created by User16 on 2018/11/27.
+//  Copyright © 2018 User16. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
